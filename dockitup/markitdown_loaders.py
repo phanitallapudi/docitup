6c767c4a15163f0b023281d263339797ba89944c
Panindhra <phanitallapudi@gmail.com>
2024-12-31 23:40:07 +0530

    add: added loaders for markitdown

--- dockitup/markitdown_loaders.py
+++ dockitup/markitdown_loaders.py
@@ -1,20 +1,21 @@
-from docling.document_converter import DocumentConverter
+from markitdown import MarkItDown # type: ignore
 from langchain_community.document_loaders.base import BaseLoader
+from langchain_core.document_loaders import BaseLoader
 from langchain_core.documents import Document as LCDocument
 from typing import Iterator, List
 
 from .base_loaders import BaseLoader as LCBaseLoader
 
-class DoclingPDFLoader(BaseLoader, LCBaseLoader):
+class MarkitdownLoader(BaseLoader, LCBaseLoader):
     def __init__(
             self, 
-            file_path: str | list[str], 
-            chunk_size: int = 1000, 
+            file_path: str | list[str],
+            chunk_size: int = 1000,
             chunk_overlap: int = 100
         ) -> None:
         super().__init__()
         self._file_paths = file_path if isinstance(file_path, list) else [file_path]
-        self._converter = DocumentConverter()
+        self.markitdown = MarkItDown()
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
 
@@ -24,14 +25,13 @@ class DoclingPDFLoader(BaseLoader, LCBaseLoader):
 
         # Process each file path
         for source in self._file_paths:
-            dl_doc = self._converter.convert(source).document
-            text = dl_doc.export_to_markdown()
+            dl_doc = self.markitdown.convert(source)
             
             # Use _text_splitter to break the document into chunks
             chunks = self._text_splitter(
                 chunk_size=self.chunk_size,
                 chunk_overlap=self.chunk_overlap,
-                documents=text,
+                documents=dl_doc.text_content,
                 metadata={"source": source}
             )
             
